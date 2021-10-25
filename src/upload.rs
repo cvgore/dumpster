bb45694de427f859bdb6019e443eac04060e3cf1
Kacper Palka <kacper@cvgo.re>
2021-10-25 21:32:51 +0200

    allow all filenames
    0.1.0-alpha.2

--- src/upload.rs
+++ src/upload.rs
@@ -3,8 +3,8 @@ use std::ffi::OsStr;
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use rocket::{State};
-use rocket::form::{Form};
+use rocket::State;
+use rocket::form::Form;
 use rocket::fs::TempFile;
 use rocket::http::Status;
 use rocket::serde::json::{json, Value};
@@ -16,6 +16,73 @@ use crate::user::User;
 #[derive(FromForm, Debug)]
 pub struct UploadData<'r> {
     file: TempFile<'r>,
+}
+
+fn sanitize_filename<'r>(given_filename: impl AsRef<OsStr>) -> Option<String> {
+    #[cfg(not(unix))]
+        let (bad_char, bad_name) = {
+        static BAD_CHARS: &[char] = &[
+            // Microsoft says these are invalid.
+            '.', '<', '>', ':', '"', '/', '\\', '|', '?', '*',
+
+            // `cmd.exe` treats these specially.
+            ',', ';', '=',
+
+            // These are treated specially by unix-like shells.
+            '(', ')', '&', '#',
+        ];
+
+        // Microsoft says these are reserved.
+        static BAD_NAMES: &[&str] = &[
+            "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4",
+            "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2",
+            "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+        ];
+
+        let bad_char = |c| BAD_CHARS.contains(&c) || c.is_control();
+        let bad_name = |n| BAD_NAMES.contains(&n);
+        (bad_char, bad_name)
+    };
+
+    #[cfg(unix)]
+        let (bad_char, bad_name) = {
+        static BAD_CHARS: &[char] = &[
+            // These have special meaning in a file name.
+            '.', '/', '\\',
+
+            // These are treated specially by shells.
+            '<', '>', '|', ':', '(', ')', '&', ';', '#', '?', '*',
+        ];
+
+        let bad_char = |c| BAD_CHARS.contains(&c) || c.is_control();
+        let bad_name = |_| false;
+        (bad_char, bad_name)
+    };
+
+    // Get the file name as a `str` without any extension(s).
+    let file_name = std::path::Path::new(&given_filename)
+        .file_name()
+        .and_then(|n| n.to_str())
+        .and_then(|n| n.split(bad_char).filter(|s| !s.is_empty()).next())?;
+
+    // At this point, `file_name` can't contain `bad_chars` because of
+    // `.split()`, but it can be empty or reserved.
+    if file_name.is_empty() || bad_name(file_name) {
+        return None;
+    }
+
+    let extension = std::path::Path::new(&given_filename)
+        .extension()
+        .and_then(|x| x.to_str())
+        .and_then(|x| x.split(bad_char).filter(|s| !s.is_empty()).next())?;
+
+    // At this point, `file_name` can't contain `bad_chars` because of
+    // `.split()`, but it can be empty or reserved.
+    if extension.is_empty() || bad_name(extension) {
+        return Some(file_name.into());
+    }
+
+    Some(format!("{}.{}", file_name, extension))
 }
 
 fn guess_scope_from_filename(filename: impl AsRef<OsStr>, prefixes: &HashMap<Arc<str>, Arc<User>>) -> (FileScope, Option<Arc<User>>) {
@@ -59,7 +126,19 @@ pub async fn upload(mut form: Form<UploadData<'_>>, state: &State<AppState>) -> 
         })));
     }
 
-    let filename = file.name().unwrap().to_string();
+    let filename = file.raw_name()
+        .map(|x| x.dangerous_unsafe_unsanitized_raw())
+        .map(|x| x.to_string());
+
+    if filename.as_ref().is_none() {
+        log::info!("tried to upload file without filename or invalid filename");
+
+        return Err((Status::BadRequest, json!({
+            "error": "filename empty or invalid"
+        })));
+    }
+
+    let filename = filename.unwrap();
 
     if filename.len() > 64 {
         log::info!("tried to upload file with too long filename");
@@ -69,25 +148,17 @@ pub async fn upload(mut form: Form<UploadData<'_>>, state: &State<AppState>) -> 
         })));
     }
 
-    if file.content_type().is_none() {
-        log::info!("tried to upload file without content type");
+    let filename = sanitize_filename(filename);
+
+    if filename.as_ref().is_none() {
+        log::warn!("tried to upload file with invalid filename chars");
 
         return Err((Status::BadRequest, json!({
-            "error": "missing content type"
+            "error": "invalid filename"
         })));
     }
 
-    let content_type = file.content_type().unwrap();
-
-    if content_type.extension().is_none() {
-        log::info!("tried to upload file with unknown extension or content type");
-
-        return Err((Status::BadRequest, json!({
-            "error": "unknown content type"
-        })));
-    }
-
-    let ext = content_type.extension().unwrap();
+    let filename = filename.unwrap();
 
     let ts = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -98,10 +169,8 @@ pub async fn upload(mut form: Form<UploadData<'_>>, state: &State<AppState>) -> 
             &filename, &state.prefix_map,
         );
 
-        let filename = format!("{}-{}", filename, ts.as_millis());
-        let path = scope.get_path_to_file(&filename, user);
-
-        path.with_extension(ext.to_string())
+        let filename = format!("{}-{}", ts.as_millis(), filename);
+        scope.get_path_to_file(&filename, user)
     };
 
 
